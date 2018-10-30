09da9d8eebf9bea7d2e3c300196986be45956880
Wayde Lyle <wayde@bonsaiworks.co.za>
2018-10-30 14:25:28 +0200

    deploy

---
+++ precache-manifest.25a01b7e73e2ef4feb60095599cae26d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "859eacc480f25650a49f",
+    "url": "/js/chunk-vendors.272e5479.js"
+  },
+  {
+    "revision": "67ee7e3549576e72b76c",
+    "url": "/js/app.994fe383.js"
+  },
+  {
+    "revision": "da1eaa38473577995c90",
+    "url": "/js/about.8a391d75.js"
+  },
+  {
+    "revision": "5ad97f8ff15de7d387cbf45bc09f06b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67ee7e3549576e72b76c",
+    "url": "/css/app.55aea04b.css"
+  }
+];
